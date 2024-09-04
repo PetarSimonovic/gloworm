1c812ab2faaed9bc7d1d1d458e753830de67e595
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2024-09-04 11:18:31 +0100

    Move textarea assignment to beforEach in CodeInput spec

--- src/components/CodeInput/CodeInput.test.tsx
+++ src/components/CodeInput/CodeInput.test.tsx
@@ -5,21 +5,20 @@ import { CodeInput } from "./CodeInput";
 const onEnterPress = mock();
 
 let container: HTMLElement;
+let textarea: HTMLTextAreaElement;
 
 describe("CodeInput", () => {
   beforeEach(() => {
     const rendered = render(<CodeInput onEnterPress={onEnterPress} />);
     container = rendered.container;
+    textarea = container.querySelector("textarea")!;
   });
   test("renders a textarea", () => {
-    const textarea = container.querySelector("textarea");
     expect(textarea).not.toBeNull();
   });
 
   describe("when Enter key is pressed", () => {
-    test("should add a new line", () => {
-      const textarea = container.querySelector("textarea");
-
+    test("adds a new line", () => {
       fireEvent.change(textarea!, { target: { value: "Hello" } });
       expect(textarea!.value).toBe("Hello");
 
@@ -27,9 +26,7 @@ describe("CodeInput", () => {
       expect(textarea!.value).toBe("Hello\n");
     });
 
-    test("should call onEnterPress", () => {
-      const textarea = container.querySelector("textarea");
-
+    test("calls onEnterPress", () => {
       fireEvent.change(textarea!, { target: { value: "Hello" } });
 
       // check onEnterPress is called
